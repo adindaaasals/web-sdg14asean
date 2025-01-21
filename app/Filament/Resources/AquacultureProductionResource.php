35953bb3908f42c9d53a3f255e9135cd0b615408
adindaaasals <adindaaasals28@gmail.com>
2025-01-21 19:39:35 +0700

    update aquaculture production

--- app/Filament/Resources/AquacultureProductionResource.php
+++ app/Filament/Resources/AquacultureProductionResource.php
@@ -44,7 +44,7 @@ class AquacultureProductionResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                // Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
